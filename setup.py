e023b1f859b4c09bafb60e1e35bb03fe46739365
Shidiq <shidiq.hidayat@gmail.com>
2021-05-25 13:06:25 +0700

    pull to nanosense

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ Created on Tue Jul 14 18:32:47 2020
 from setuptools import setup
 
 setup(name='nanolib',
-      version='0.0.18',
+      version='0.0.19',
       description='The Data Science Assistant from NANOSENSE',
-      url='https://github.com/Shidiq/nanolib.git',
+      url='https://github.com/nanosenseinstrument/nanolib.git',
       author='Shidiq Nur Hidayat',
       author_email='s.hidayat@nanosense-id.com',
       license='MIT',
